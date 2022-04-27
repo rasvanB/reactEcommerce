edc33595b88a678bcf181d5a43399930b3c104c7
BosneaguRazvan <rasvanb@ubuntu20.4>
2022-04-28 00:54:48 +0300

    added sign-in page with sign-in form component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
+import SignInPage from "./pages/sign-in-page/sign-in-page.component";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
@@ -11,7 +12,8 @@ function App() {
             <Header></Header>
             <Routes>
                 <Route exact path="/" element={<HomePage />} />
-                <Route exact path="/shop" element={<ShopPage />}></Route>
+                <Route exact path="/shop" element={<ShopPage />} />
+                <Route exact path="/signin" element={<SignInPage />} />
             </Routes>
         </div>
     );
