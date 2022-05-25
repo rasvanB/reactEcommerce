078db84786167e43f0750192548e249eefeb071c
rasvanB <not.obelix@gmail.com>
2022-05-25 20:23:47 +0300

    more redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import "./index.css";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
@@ -8,15 +10,17 @@ import { CategoriesProvider } from "./contexts/products.context";
 import { CartProvider } from "./contexts/cart.context";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </React.StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
