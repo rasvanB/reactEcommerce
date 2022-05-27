0d55e3743b2e782f916e5d04f6c43afcb9650e43
rasvanB <not.obelix@gmail.com>
2022-05-27 13:55:30 +0300

    added memoization for categories selector with reselect library

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categoriesArray.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categories) => categories.categoriesArray
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categoriesArray) =>
+    categoriesArray.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
