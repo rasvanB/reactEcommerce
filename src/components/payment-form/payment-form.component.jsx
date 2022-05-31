53b7c82c5006d3e97209e0a1152d46842b9f6955
rasvanB <not.obelix@gmail.com>
2022-05-31 21:47:19 +0300

    netlify to .netlify

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -9,10 +9,20 @@ const PaymentForm = () => {
     if (!stripe || !elements) {
       return;
     }
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        amount: "500",
+      }),
+    }).then((res) => res.json());
+    console.log(response);
   };
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit card payment</h2>
         <CardElement />
         <CustomButton isInverted>Pay Now</CustomButton>
