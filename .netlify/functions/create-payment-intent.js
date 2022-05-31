55161cb220a10bd76c455daea41ad9341eebbba9
rasvanB <not.obelix@gmail.com>
2022-05-31 22:03:36 +0300

    create-payment-intent FIXED

--- .netlify/functions/create-payment-intent.js
+++ .netlify/functions/create-payment-intent.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
 exports.handler = async (event) => {
   try {
     const { amount } = JSON.parse(event.body);
