36cee9c1699b50132234825617f8ae9d28e2639b
rasvanB <not.obelix@gmail.com>
2022-06-03 22:42:29 +0300

    converted categories actions to typescript

--- src/store/categories/categories-reducer.ts
+++ src/store/categories/categories-reducer.ts
@@ -4,11 +4,24 @@ const INITIAL_STATE = {
   error: null,
 };
 
-export const CATEGORIES_ACTION_TYPES = {
-  FETCH_CATEGORIES_START: "FETCH_CATEGORIES_START",
-  FETCH_CATEGORIES_SUCCESS: "FETCH_CATEGORIES_SUCCESS",
-  FETCH_CATEGORIES_FAILED: "FETCH_CATEGORIES_FAILED",
+export type CategoryItem = {
+  id: number;
+  name: string;
+  imageUrl: string;
+  price: Number;
 };
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+};
+
+export enum CATEGORIES_ACTION_TYPES {
+  FETCH_CATEGORIES_START = "FETCH_CATEGORIES_START",
+  FETCH_CATEGORIES_SUCCESS = "FETCH_CATEGORIES_SUCCESS",
+  FETCH_CATEGORIES_FAILED = "FETCH_CATEGORIES_FAILED",
+}
 
 export const categoriesReducer = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
---
+++ src/store/categories/categories.action.ts
@@ -0,0 +1,48 @@
+import { createAction } from "../store.utils";
+import { getCategoriesAndDocuments } from "../../firebase/firebase.utils";
+import { Action, ActionWithPayload } from "../store.utils";
+import { ThunkDispatch } from "redux-thunk";
+import { AnyAction } from "redux";
+import { CATEGORIES_ACTION_TYPES } from "./categories-reducer";
+import { Category } from "./categories-reducer";
+
+export type FetchCategoriesStart =
+  Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
+
+export type FetchCategoriesSuccess = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+  Category[]
+>;
+
+export type FetchCategoriesFailed = ActionWithPayload<
+  CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+  Error
+>;
+
+export type CategoryAction =
+  | FetchCategoriesStart
+  | FetchCategoriesSuccess
+  | FetchCategoriesFailed;
+
+export const fetchCategoriesStart = (): FetchCategoriesStart =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (
+  categories: Category[]
+): FetchCategoriesSuccess =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categories);
+
+export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
+  createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync =
+  () =>
+  async (dispatch: ThunkDispatch<{}, {}, AnyAction>): Promise<void> => {
+    dispatch(fetchCategoriesStart());
+    try {
+      const categoriesArray = await getCategoriesAndDocuments();
+      dispatch(fetchCategoriesSuccess(categoriesArray as Category[]));
+    } catch (error) {
+      dispatch(fetchCategoriesFailed(error as Error));
+    }
+  };
