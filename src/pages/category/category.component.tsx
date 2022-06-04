8dceba52f8c7a26d1c9871df8c7f9c8d7383eca0
rasvanB <not.obelix@gmail.com>
2022-06-05 01:33:54 +0300

    typed category

---
+++ src/pages/category/category.component.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect, Fragment } from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
+
+import {
+  selectCategoriesMap,
+  selectLoadingState,
+} from "../../store/categories/categories.selector";
+import "./category.styles.scss";
+
+type CategoryParams = {
+  category: string;
+};
+const Category = () => {
+  const { category } = useParams<keyof CategoryParams>() as CategoryParams;
+  const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectLoadingState);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <Fragment>
+      <h2 className="category-title">{category.toUpperCase()}</h2>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="category-container">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+      )}
+    </Fragment>
+  );
+};
+
+export default Category;
