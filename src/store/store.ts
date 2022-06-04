52d9306ef4018ca8e2c8aef951d8834c3f6740ee
rasvanB <not.obelix@gmail.com>
2022-06-04 23:59:20 +0300

    typed store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,15 +1,9 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import { rootReducer } from "./root-reducer";
 import thunk from "redux-thunk";
-
-const middleware = [
-  process.env.NODE_ENV === "development" && logger,
-  thunk,
-].filter(Boolean);
-
 const persistConfig = {
   key: "root",
   storage,
@@ -20,7 +14,10 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware: middleware,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      process.env.NODE_ENV === "development" ? [logger, thunk] : [thunk]
+    ),
   devTools: true,
 });
 
